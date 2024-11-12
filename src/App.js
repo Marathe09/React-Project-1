a5a2ad89c59faca9797ccd8c675f31b89cf3bbc5
Deep Marathe <marathedeep09@gmail.com>
2024-11-12 15:20:42 +0530

    one

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <>
+    <Navbar title="Textutils" About="About"/>
+    </>    
+  );
+}
+
+export default App;
